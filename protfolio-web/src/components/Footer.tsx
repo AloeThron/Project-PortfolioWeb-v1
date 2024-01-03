b3f60d67d56a95a94f6acc7130632dab0b902eab
AloeThron <lxe.tr.deat@gmail.com>
2024-01-04 02:18:08 +0700

    part contact

--- protfolio-web/src/components/Footer.tsx
+++ protfolio-web/src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer({}: Props) {
     <div className='bg-secondary pt-10 pb-4'>
       <div className='container mx-auto text-center'>
         {/* copyright */}
-        <div className='text-muted-foreground'>Copyright &copy; Patyos. All rights reserved.</div>
+        <div className='text-muted-foreground'>Copyright &copy; 2024. All rights reserved.</div>
       </div>
     </div>
   )
